5a418466a422aaed46146e35c789dcdcaa274cbe
chiwisdp <chiwisdp@gmail.com>
2025-08-13 11:42:50 -0700

    Minor fixes to case studies

---
+++ data/projectData.ts
@@ -0,0 +1,110 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Cyber Frog",
+    description: "A Unity3d game about a cyberpunk frog on a hover bike.",
+    images: [
+      "/projectImages/rocksmith/rLogo.png",
+      "/projectImages/rocksmith/rAnim.gif",
+    ],
+  },
+  {
+    id: 2,
+    title: "Indeed VR Interview",
+    description: "A cross-platform multiplayer FPS from Ubisoft.",
+    images: [
+      "/projectImages/rocksmith/rLogo.png",
+      "/projectImages/xdefiant/xdefiantGame.png",
+    ],
+  },
+  {
+    id: 3,
+    title: "Game Builder",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 4,
+    title: "Cold Email",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 5,
+    title: "Star Wars VR Fan Game",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 6,
+    title: "Shaq Sky Slam",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 7,
+    title: "Dig Quest Israel",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 8,
+    title: "Dynamice",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 9,
+    title: "Kraken Rum",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 10,
+    title: "TypeSet",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 11,
+    title: "Spectre",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+  {
+    id: 12,
+    title: "Kid The World Saver",
+    description: "Redesign of the application flow for Indeed.",
+    images: [
+      "/projectImages/indeed/indeedLogo.png",
+      "/projectImages/indeed/indeedDesignJam.png",
+    ],
+  },
+];
