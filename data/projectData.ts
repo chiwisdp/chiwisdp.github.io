106bd8775d5b1197eebfde06d66291c65443c7c8
chiwisdp <chiwisdp@gmail.com>
2025-08-15 00:48:48 -0700

    all of projects page

--- data/projectData.ts
+++ data/projectData.ts
@@ -2,109 +2,130 @@ export const projects = [
   {
     id: 1,
     title: "Cyber Frog",
-    description: "A Unity3d game about a cyberpunk frog on a hover bike.",
+    description:
+      "A Unity3d game about a cyberpunk frog on a hover bike. This is a personal project that I did on my spare time. Its a 3d runner where your hover bike can change gravity to drive on walls to avoid obstacles.",
     images: [
-      "/projectImages/rocksmith/rLogo.png",
-      "/projectImages/rocksmith/rAnim.gif",
+      "/projectImages/cryberfrog/cryber1.png",
+      "/projectImages/cryberfrog/cryber2.png",
+      "/projectImages/cryberfrog/cryber3.png",
+      "/projectImages/cryberfrog/cryber4.png",
     ],
+    youtubeVideoId: "m3qtF0B_9XQ",
   },
   {
     id: 2,
     title: "Indeed VR Interview",
-    description: "A cross-platform multiplayer FPS from Ubisoft.",
+    description:
+      "This was done at a internal Indeed hackathon. This was a virtual reality interview experience where the user can practice their interview skills in a VR environment. This was done over a 3 day period in Unity3D.",
     images: [
-      "/projectImages/rocksmith/rLogo.png",
-      "/projectImages/xdefiant/xdefiantGame.png",
+      "/projectImages/indeedVR/interview1.png",
+      "/projectImages/indeedVR/interview2.png",
+      "/projectImages/indeedVR/interview3.png",
     ],
+    youtubeVideoId: "y5L3MsbjRNA",
   },
   {
     id: 3,
     title: "Game Builder",
-    description: "Redesign of the application flow for Indeed.",
+    description:
+      "I was responsible for the UX and UI of this project. Build and play games with your friends! Make a 3D platformer in 10 minutes, or spend hours jamming on the next indie hit. Game Builder is designed for all skill levels, from players ready to build their first game to designers looking for a faster way to realize their ideas.",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/gameBuilder/header.jpg",
+      "/projectImages/gameBuilder/2.jpg",
+      "/projectImages/gameBuilder/3.jpg",
     ],
+    youtubeVideoId: "v84kWmr0LMM",
   },
   {
     id: 4,
     title: "Cold Email",
-    description: "Redesign of the application flow for Indeed.",
+    description:
+      "In Cold Email, you need to fire off emails as fast as you can before your computer explodes from malware. The send button has a nasty tendency to wander in the Game Jam title, though, so it may be difficult to get out all of those necessary emails before your PC bursts into flames. Do try, though, as your game, and your livelihood, depends on it.",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/coldEmail/0.gif",
+      "/projectImages/coldEmail/1.png",
+      "/projectImages/coldEmail/3.png",
+      "/projectImages/coldEmail/5.png",
     ],
+    youtubeVideoId: "Jz2JqPfVD4s",
   },
   {
     id: 5,
-    title: "Star Wars VR Fan Game",
-    description: "Redesign of the application flow for Indeed.",
+    title: "Star Wars VR",
+    description:
+      "R2 Story : A Star Wars VR Fan game. This is a fan project and will not be distributed. This is just me playing around with Unity3D and an Oculus DK 2 kit. Lucasfilm and Disney Own the rights to the Star Wars brand.",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/swvr/TitleSWVR.png",
+      "/projectImages/swvr/frontSWVR.png",
+      "/projectImages/swvr/BackSWVR.png",
     ],
+    youtubeVideoId: "ZQOZLxbX3f4",
   },
   {
     id: 6,
     title: "Shaq Sky Slam",
-    description: "Redesign of the application flow for Indeed.",
+    description:
+      "Get ready for the most over the top extreme sport ever created. Skydive with a basketball in hand, plunge through the stratosphere at terminal velocity, grabbing coins and diving through hoops along the way. Dive bomb toward the basketball court below, lock in on your target, and slam dunk that ball!",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/shaq/sss3.png",
+      "/projectImages/shaq/sss0.png",
+      "/projectImages/shaq/sss1.png",
+      "/projectImages/shaq/sss2.png",
     ],
+
+    youtubeVideoId: "vNjKb3lIww8",
   },
   {
     id: 7,
     title: "Dig Quest Israel",
-    description: "Redesign of the application flow for Indeed.",
+    description:
+      "Become an archeologist, and use your device to explore Israel’s past! Dig up the 2,000-year-old Lod Mosaic, and piece together the Dead Sea Scrolls. A collaboration between Crater House, Maya Cohan Productions, and the Israel Antiquities Authority!",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/digQuest/dqScreen3.png",
+      "/projectImages/digQuest/dqscreen.png",
+      "/projectImages/digQuest/dqScreen1.png",
     ],
+    youtubeVideoId: "",
   },
   {
     id: 8,
     title: "Dynamice",
-    description: "Redesign of the application flow for Indeed.",
+    description:
+      "Players take turns running the maze and can see the footprints left by previous turns. The question to ask: is the other player running towards the cheese, or leading you into a trap?",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/dynamice/DynamiceHeader.png",
+      "/projectImages/dynamice/Dyna0.png",
+      "/projectImages/dynamice/dyna2.png",
     ],
+    youtubeVideoId: "",
   },
   {
     id: 9,
-    title: "Kraken Rum",
-    description: "Redesign of the application flow for Indeed.",
+    title: "TypeSet",
+    description:
+      "Type Set is a game about moving a conversation along, literally. The player physically manipulates the words on screen through touch gestures. Just like a normal conversation depending on which words you emphasize it can change the tone and direction of the conversation. Emphasis is placed on certain words by making them bigger /smaller or rotating them. The conversation changes depending on the way players solve the puzzles ( i.e. emphasizing certain words to change the tone of the conversation in an angry or nice way).",
     images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
+      "/projectImages/typeSet/tsHeader.jpg",
+      "/projectImages/typeSet/5.png",
+      "/projectImages/typeSet/6.png",
+      "/projectImages/typeSet/7.png",
     ],
+    youtubeVideoId: "DFrrzchmqK4",
   },
   {
     id: 10,
-    title: "TypeSet",
-    description: "Redesign of the application flow for Indeed.",
-    images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
-    ],
+    title: "Spectre",
+    description:
+      "Spectre is a recombinant narrative platformer, a game that tells the story of an individual's life. As you navigate through his specific recollections, similarly themed events will glow brightly. If you succeed in these moments of play and follow a glowing path, you will find a theme uniting his experience, and uncover a little more of his fading memory. If not, your nightly story will end in confusion.",
+    images: ["/projectImages/spectre/spectre.png"],
+    youtubeVideoId: "_0SdfMBXPDQ",
   },
   {
     id: 11,
-    title: "Spectre",
-    description: "Redesign of the application flow for Indeed.",
-    images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
-    ],
-  },
-  {
-    id: 12,
     title: "Kid The World Saver",
-    description: "Redesign of the application flow for Indeed.",
-    images: [
-      "/projectImages/indeed/indeedLogo.png",
-      "/projectImages/indeed/indeedDesignJam.png",
-    ],
+    description:
+      "Move blocks around to guide Kid to the end of the level! IGF Student Showcase Finalist 2009.",
+    images: ["/projectImages/kid/kid1.jpg", "/projectImages/kid/kid2.jpg"],
+    youtubeVideoId: "hBoAGmgIKOg",
   },
 ];
